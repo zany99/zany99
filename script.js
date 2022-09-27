f7ed44b67a8e65aa9af1ed8cbf4a67c68a42ca39
Nikita Ignatenko <gooddaymyfriend3@gmail.com>
2022-09-27 12:10:11 +0300

    google

---
+++ script.js
@@ -0,0 +1,6 @@
+const changeBtn = document.getElementById('change__btn')
+const changeLogo = document.getElementById('change__logo')
+
+changeBtn.addEventListener('click', () => {
+    changeLogo.src="./logo_1.png"
+})
